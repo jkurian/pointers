b01ed257a678faccb8d1ccfdb438aa2506a9fc56
Jerry Kurian <jerrykurian@protonmail.com>
2020-09-19 19:14:15 -0400

    Add marker to ensure drop check for T

--- src/rc.rs
+++ src/rc.rs
@@ -1,13 +1,30 @@
 use crate::cell::Cell;
+use std::marker::PhantomData;
 use std::ptr::NonNull;
 
-pub struct InnerRc<T> {
+pub struct RcInner<T> {
     value: T,
     count: Cell<usize>,
 }
 
 pub struct Rc<T> {
-    inner: NonNull<InnerRc<T>>,
+    inner: NonNull<RcInner<T>>,
+    _marker: PhantomData<RcInner<T>>,
+}
+
+impl<T> Rc<T> {
+    pub fn new(value: T) -> Self {
+        let inner = Box::new(RcInner {
+            value,
+            count: Cell::new(0),
+        });
+
+        Rc {
+            /// SAFETY: Box does not give us a null pointer
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
+            _marker: PhantomData,
+        }
+    }
 }
 
 impl<T> Clone for Rc<T> {
@@ -15,7 +32,10 @@ impl<T> Clone for Rc<T> {
         let inner = unsafe { self.inner.as_ref() };
         let c = inner.count.get() + 1;
         inner.count.set(c);
-        Rc { inner: self.inner }
+        Rc {
+            inner: self.inner,
+            _marker: PhantomData,
+        }
     }
 }
 
